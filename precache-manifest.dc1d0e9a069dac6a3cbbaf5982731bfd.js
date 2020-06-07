604e77f0ec4403216c51279ede48d406f65a139b
exbtrg <exbtrg@gmail.com>
2020-06-07 05:44:50 +0300

    Updates

---
+++ precache-manifest.dc1d0e9a069dac6a3cbbaf5982731bfd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df39afa1f08211ab406e9f3b97819ef",
+    "url": "/myCodeExampleToDoList/index.html"
+  },
+  {
+    "revision": "eb420b8ee27d7a24c4ec",
+    "url": "/myCodeExampleToDoList/static/css/main.7b808663.chunk.css"
+  },
+  {
+    "revision": "1742eb21ea600d08e0a7",
+    "url": "/myCodeExampleToDoList/static/js/2.9277718a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myCodeExampleToDoList/static/js/2.9277718a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb420b8ee27d7a24c4ec",
+    "url": "/myCodeExampleToDoList/static/js/main.9bc86cc3.chunk.js"
+  },
+  {
+    "revision": "176fa0a840ec4f00ea4b",
+    "url": "/myCodeExampleToDoList/static/js/runtime-main.7592db41.js"
+  },
+  {
+    "revision": "0135ef52217674165972ffd8c0d41c78",
+    "url": "/myCodeExampleToDoList/static/media/delete.0135ef52.svg"
+  },
+  {
+    "revision": "70435f0b92cf035b8826704c97ed3c63",
+    "url": "/myCodeExampleToDoList/static/media/done.70435f0b.svg"
+  },
+  {
+    "revision": "a0d3a50fb69f01054a64e94d5deca6c2",
+    "url": "/myCodeExampleToDoList/static/media/important.a0d3a50f.svg"
+  }
+]);
