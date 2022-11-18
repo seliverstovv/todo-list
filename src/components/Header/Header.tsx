2e178db9ea6ec786733a5de33b6316bef05852a1
vseliverstov <vseliverstov.work@gmail.com>
2022-11-19 02:05:35 +0400

    added default value for theme checkbox / updated reame md

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent } from "react"
 import styled from "@emotion/styled"
 import { useAppDispath, useAppSelector } from "store/hooks"
 import { todoItemsCountsSelector } from "features/todo/selectors"
+import { themeSelector } from "features/UI/selectors"
 import { setTheme } from "features/UI/UISlice"
 import Typography from "UI/Typography"
 import styles from "./styles"
@@ -9,6 +10,7 @@ import styles from "./styles"
 const Header = ({ className }: { className?: string }) => {
   const dispatch = useAppDispath()
   const { doneCount, progressCount } = useAppSelector(todoItemsCountsSelector)
+  const theme = useAppSelector(themeSelector)
 
   const themeChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.checked) {
@@ -23,7 +25,7 @@ const Header = ({ className }: { className?: string }) => {
       <Typography className="title" tag="h2" size="xxl">
         My Todo List
       </Typography>
-      <input type="checkbox" onChange={themeChangeHandler} />
+      <input type="checkbox" checked={theme === "dark"} onChange={themeChangeHandler} />
       <Typography tag="h3" size="xl">
         {progressCount} more to do, {doneCount} done
       </Typography>
