ae3fe213092f5bed03468dd3602ed571dd8ae7f9
vseliverstov <vseliverstov.work@gmail.com>
2023-03-01 11:55:04 +0400

    fix ident

--- src/UI/Portal/index.tsx
+++ src/UI/Portal/index.tsx
@@ -2,17 +2,17 @@ import { ReactNode, useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 
 const Portal = ({ children }: { children: ReactNode }) => {
-  const [container] = useState(() => document.createElement("div"))
+    const [container] = useState(() => document.createElement("div"))
 
-  useEffect(() => {
-    document.body.appendChild(container)
-    return () => {
-      document.body.removeChild(container)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    useEffect(() => {
+        document.body.appendChild(container)
+        return () => {
+            document.body.removeChild(container)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-  return ReactDOM.createPortal(children, container)
+    return ReactDOM.createPortal(children, container)
 }
 
 export default Portal
